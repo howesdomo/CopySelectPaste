ba34d4a4fed6d52be0ba6345498ef09325aa3c78
howesdomo <164572429@qq.com>
2020-03-14 16:45:27 +0800

    V 1.0.1 1 采用FiraCode字体, 更好地区分 tab 和空格 2 LINQ小程序搬迁至此

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -23,14 +23,41 @@ namespace Client
         {
             InitializeComponent();
 
+            initUI();
+            initEvent();
+        }
+
+        private void initUI()
+        {
             this.Title = $"{this.Title} - {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()}";
 
-            initEvent();
+            #region 设置 Fira Code 字体
+
+            var fontFile = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources", "TTF", "FiraCode-Regular.ttf");
+            var fontFamily_Fira_Code = Util.FontFamilyUtils.GetFontFamily_TypeOf_System_Windows_Media(fontFile);
+
+            txtInput.FontFamily = fontFamily_Fira_Code;
+            txtOutput0.FontFamily = fontFamily_Fira_Code;
+            txtOutput_SQL_0.FontFamily = fontFamily_Fira_Code;
+            txtOutput_SQL_1.FontFamily = fontFamily_Fira_Code;
+            txtOutput_SQL_2.FontFamily = fontFamily_Fira_Code; txtOutput_SQL_2_Arg1.FontFamily = fontFamily_Fira_Code; txtOutput_SQL_2_Arg2.FontFamily = fontFamily_Fira_Code;
+
+            txtOutput_CSharp_0.FontFamily = fontFamily_Fira_Code;
+            txtOutput_CSharp_1.FontFamily = fontFamily_Fira_Code; txtOutput_CSharp_1_Arg.FontFamily = fontFamily_Fira_Code;
+            txtOutput_CSharp_2.FontFamily = fontFamily_Fira_Code;
+            txtOutput_CSharp_3.FontFamily = fontFamily_Fira_Code;
+            txtOutput_CSharp_4.FontFamily = fontFamily_Fira_Code;
+
+            #endregion
         }
 
         private void initEvent()
         {
+            // 输入参数
             this.txtInput.TextChanged += txtInput_TextChanged;
+            this.txtOutput_CSharp_1_Arg.TextChanged += txtInput_TextChanged;
+            this.txtOutput_SQL_2_Arg1.TextChanged += txtInput_TextChanged;
+            this.txtOutput_SQL_2_Arg2.TextChanged += txtInput_TextChanged;
 
             // CheckBox
             this.cbTabNewLine.Click += checkBox_Click;
@@ -43,14 +70,39 @@ namespace Client
             this.rbRemoveWhiteSpace.Checked += radioButton_Checked;
 
             // 除内容空格
-            // this.rbNotTrim.Click += radioButton_Click; // 先命中 Checked, 再命中 Click, 故只注册 Check 就好了
             this.rbNotTrim.Checked += radioButton_Checked;
             this.rbTrimStart.Checked += radioButton_Checked;
             this.rbTrimEnd.Checked += radioButton_Checked;
             this.rbTrim.Checked += radioButton_Checked;
             this.rbTrimAdv.Checked += radioButton_Checked;
+
+            // 双击复制到粘贴板
+            tab0.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_SQL_0.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_SQL_1.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_SQL_2.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_CSharp_0.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_CSharp_1.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_CSharp_2.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_CSharp_3.MouseDoubleClick += tabX_MouseDoubleClick;
+            tab_CSharp_4.MouseDoubleClick += tabX_MouseDoubleClick;
         }
 
+        private void tabX_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TabItem)
+            {
+                copyToClipboard((sender as TabItem).Content);
+            }
+        }
+
+        private void copyToClipboard(object sender)
+        {
+            if (sender is TextBox)
+            {
+                Clipboard.SetText((sender as TextBox).Text);
+            }
+        }
 
         private void checkBox_Click(object sender, RoutedEventArgs e)
         {
@@ -81,19 +133,26 @@ namespace Client
 
             if (text.IsNullOrWhiteSpace() == true)
             {
-                txtInputInfo.Text = "空值";
+                this.txtInputInfo.Text = "空值";
+
                 this.txtOutputInfo.Text = string.Empty;
+                this.txtOutput0.Text = string.Empty;
+                this.txtOutput_SQL_0.Text = string.Empty;
+                this.txtOutput_SQL_1.Text = string.Empty;
+                this.txtOutput_SQL_2.Text = string.Empty;
+                this.txtOutput_CSharp_0.Text = string.Empty;
+                this.txtOutput_CSharp_1.Text = string.Empty;
+                this.txtOutput_CSharp_2.Text = string.Empty;
+                this.txtOutput_CSharp_3.Text = string.Empty;
+                this.txtOutput_CSharp_4.Text = string.Empty;
                 return;
             }
-            
-            
-            StringBuilder sbInfo = new StringBuilder();           
 
-            var query = text.Split('\n').AsQueryable<string>();
-            sbInfo.Append($"源文件共 {query.Count()} 行; ");
+            StringBuilder sbInfo = new StringBuilder();
+            var matchNewLineSymbol = System.Text.RegularExpressions.Regex.Matches(text, "[\r]?\n");
+            sbInfo.Append($"源文件共 {matchNewLineSymbol.Count + 1} 行; ");
 
-
-
+            var query = text.Split('\r', '\n').AsQueryable<string>();
             if (cbTabNewLine.IsChecked.Value)
             {
                 List<string> temp = new List<string>();
@@ -149,9 +208,14 @@ namespace Client
 
 
             this.txtOutput0.Text = calc(list);
-            this.txtOutput1.Text = SQL_IN条件参数(list);
-            this.txtOutput2.Text = SQL_插入临时表(list);
-            this.txtOutput3.Text = CSharp_插入List(list);
+            this.txtOutput_SQL_0.Text = SQL_IN条件参数(list);
+            this.txtOutput_SQL_1.Text = SQL_插入临时表(list);
+            this.txtOutput_SQL_2.Text = SQL_Insert语句(list);
+            this.txtOutput_CSharp_0.Text = CSharp_插入List(list);
+            this.txtOutput_CSharp_1.Text = CSharp_DataTable(list);
+            this.txtOutput_CSharp_2.Text = CSharp_rDataTable(list);
+            this.txtOutput_CSharp_3.Text = CSharp_rDataTableHowe(list);
+            this.txtOutput_CSharp_4.Text = CSharp_CopyModel(list);
         }
 
         private string calc(List<string> l)
@@ -196,6 +260,58 @@ namespace Client
             return r.Substring(0, r.Length - 2); // 去掉最后的两个符号
         }
 
+        private string SQL_Insert语句(List<string> l)
+        {
+            string insertTableName = this.txtOutput_SQL_2_Arg1.Text;
+            if (insertTableName.IsNullOrWhiteSpace())
+            {
+                insertTableName = "@tmp";
+            }
+
+            IEnumerable<string> m = this.txtOutput_SQL_2_Arg2.Text.Split(',', ' ', '\t').Where(i => i.IsNullOrWhiteSpace() == false);
+            List<int> ignoreIndexList = new List<int>();
+            foreach (var item in m)
+            {
+                if (int.TryParse(item, out int i))
+                {
+                    ignoreIndexList.Add(i);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var oneline in l)
+            {
+                sb.Append($"insert into {insertTableName} values ( ");
+                var splitedTabArr = oneline.Split('\t');
+                for (int index = 0; index < splitedTabArr.Length; index++)
+                {
+                    string item = splitedTabArr[index];
+                    if (item.Equals("null", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        sb.Append(item);
+                    }
+                    else if (ignoreIndexList.Any(j => j == index)) // 忽略用户手输的
+                    {
+                        sb.Append(item);
+                    }
+                    else
+                    {
+                        sb.Append("'").Append(item).Append("'");
+                    }
+
+                    if (index + 1 < splitedTabArr.Length)
+                    {
+                        sb.Append(", ");
+                    }
+                }
+                sb.AppendLine($" );");
+            }
+
+            var r = sb.ToString();
+            return r.Substring(0, r.Length - 2); // 去掉最后的两个符号
+        }
+
         private string CSharp_插入List(List<string> l)
         {
             StringBuilder sb = new StringBuilder();
@@ -206,6 +322,193 @@ namespace Client
 
             var r = sb.ToString();
             return r.Substring(0, r.Length - 2); // 去掉最后的两个符号
+        }
+
+        private string CSharp_DataTable(List<string> l)
+        {
+            string tabSymbol = "\t";
+
+            StringBuilder sb = new StringBuilder();
+
+            #region 创建表
+
+            sb.AppendLine($"public System.Data.DataTable CreateDataTable_X()");
+            sb.AppendLine("{");
+
+            sb.Append(tabSymbol).AppendLine("System.Data.DataTable dt = new System.Data.DataTable();");
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).AppendLine("dt.Columns.Add(\"" + item + "\");");
+            }
+
+            sb.Append(tabSymbol).AppendLine("return dt;");
+            sb.AppendLine("}");
+
+            #endregion
+
+            sb.AppendLine("");
+
+            #region 表赋值
+
+            string typeName = this.txtOutput_CSharp_1_Arg.Text;
+            if (typeName.IsNullOrWhiteSpace() == true) { typeName = "XModel"; }
+
+            sb.AppendLine($"public System.Data.DataTable GetDataTable(System.Collections.Generic.IEnumerable<{typeName}> l)");
+            sb.AppendLine("{");
+
+            sb.Append(tabSymbol).AppendLine("var dt = CreateDataTable_X();");
+            sb.Append(tabSymbol).AppendLine("foreach (var item in l)");
+            sb.Append(tabSymbol).AppendLine("{");
+            sb.Append(tabSymbol).Append(tabSymbol).AppendLine("DataRow dr = dt.NewRow();");
+            sb.Append(tabSymbol).Append(tabSymbol).AppendLine("dt.Rows.Add(dr);");
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).Append(tabSymbol).AppendLine($"dr[\"{item}\"] = item.{item};");
+            }
+
+            sb.Append(tabSymbol).AppendLine("}");
+            sb.Append(tabSymbol).AppendLine("return dt;");
+            sb.AppendLine("}");
+
+            #endregion
+
+            return sb.ToString();
+        }
+
+        private string CSharp_rDataTable(List<string> l)
+        {
+            string tabSymbol = "\t";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("foreach (DataRow dr in dt.Rows)");
+            sb.AppendLine("{");
+            sb.Append(tabSymbol).AppendLine("CA toAdd = new CA();");
+            sb.Append(tabSymbol).AppendLine("l.Add(toAdd);");
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadString(dr[\"{item}\"]);");
+            }
+
+            sb.AppendLine("}");
+            sb.AppendLine("return dt;");
+
+            return sb.ToString();
+        }
+
+        private string CSharp_rDataTableHowe(List<string> l)
+        {
+            string tabSymbol = "\t";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("foreach (DataRow dr in dt.Rows)");
+            sb.AppendLine("{");
+            sb.Append(tabSymbol).AppendLine("CA toAdd = new CA();");
+            sb.Append(tabSymbol).AppendLine("l.Add(toAdd);");
+
+            foreach (var item in l)
+            {
+                if (item.Equals("ID", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadGuid(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("OrderID", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadGuid(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("OrderItemID", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadGuid(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("Time", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadDateTime(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("Date", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadDateTime(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("Qty", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadDecimal(dr[\"{item}\"]);");
+                }
+                else if (item.EndsWith("SNP", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadDecimal(dr[\"{item}\"]);");
+                }
+                else
+                {
+                    sb.Append(tabSymbol).AppendLine($"toAdd.{item} = Util.CommonDal.ReadString(dr[\"{item}\"]);");
+                }
+            }
+
+            sb.AppendLine("}");
+            sb.AppendLine("return dt;");
+
+            return sb.ToString();
+        }
+
+        private string CSharp_DBReader(List<string> l)
+        {
+            string tabSymbol = "\t";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"public System.Data.DataTable CreateDataTable_X()");
+            sb.AppendLine("{");
+
+            sb.Append(tabSymbol).AppendLine("System.Data.DataTable dt = new System.Data.DataTable();");
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).AppendLine("dt.Columns.Add(\"" + item + "\");");
+            }
+
+            sb.Append(tabSymbol).AppendLine("return dt;");
+            sb.AppendLine("}");
+
+            sb.AppendLine("public System.Data.DataTable GetDataTable(IEnumerable<XModel> l)");
+            sb.AppendLine("{");
+
+            sb.Append(tabSymbol).AppendLine("var dt = CreateDataTable_X();");
+            sb.Append(tabSymbol).AppendLine("foreach (var item in l)");
+            sb.Append(tabSymbol).AppendLine("{");
+            sb.Append(tabSymbol).Append(tabSymbol).AppendLine("DataRow dr = dt.NewRow();");
+            sb.Append(tabSymbol).Append(tabSymbol).AppendLine("dt.Rows.Add(dr);");
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).Append(tabSymbol).AppendLine($"dr[\"{item}\"] = item.{item};");
+            }
+
+            sb.Append(tabSymbol).AppendLine("return dt;");
+            sb.Append(tabSymbol).AppendLine("}");
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        private string CSharp_CopyModel(List<string> l)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"public void UpdateData(XModel o, XModel t)");
+            sb.AppendLine("{");
+
+            string tabSymbol = "\t";
+
+            foreach (var item in l)
+            {
+                sb.Append(tabSymbol).AppendLine($"o.{item} = t.{item};");
+            }
+
+            sb.AppendLine("}");
+
+            return sb.ToString();
         }
     }
 }
